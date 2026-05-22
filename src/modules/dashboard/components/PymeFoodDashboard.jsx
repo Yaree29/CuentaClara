7df3059fa1168b22595cb087ea92e9f2ad12aa46
Yaree29 <yarlep0220@gmail.com>
2026-05-22 00:49:47 -0500

    Corrección en el home codigo base en los diferentes dashbord

--- src/modules/dashboard/components/PymeFoodDashboard.jsx
+++ src/modules/dashboard/components/PymeFoodDashboard.jsx
@@ -1,1 +1,14 @@
 //componentes del dashboard para PYME en el sector alimenticio carnicos
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PymeFoodDashboard = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Dashboard PYME Alimenticio (En construcción)</Text>
+    </View>
+  );
+};
+
+export default PymeFoodDashboard;
--- src/modules/dashboard/components/PymePrepFoodDashboard.jsx
+++ src/modules/dashboard/components/PymePrepFoodDashboard.jsx
@@ -1,1 +1,14 @@
 //componentes del dashboard para PYME en el sector alimenticio preparados
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PymePrepFoodDashboard = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Dashboard PYME Alimenticio Preparado (En construcción)</Text>
+    </View>
+  );
+};
+
+export default PymePrepFoodDashboard;

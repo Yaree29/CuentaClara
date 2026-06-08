c1f4743e224dcd5456e44ff63d0152ecf9c71106
Yaree29 <yarlep0220@gmail.com>
2026-06-08 00:06:57 -0500

    Modulo de seguridad

--- src/views/navigation/MainStackNavigator.jsx
+++ src/views/navigation/MainStackNavigator.jsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainNavigator from './MainNavigator';
 import CreateTransactionScreen from '../../modules/transctions/screens/CreateTransactionScreen';
 import ProfileScreen from '../../modules/profile/screens/ProfileScreen';
+import SecuritySettings from '../../modules/profile/screens/SecuritySettingsScreen';
 import Token2FA from '../../modules/verification/screens/Token2FA';
 
 const Stack = createNativeStackNavigator();
@@ -13,6 +14,7 @@ const MainStackNavigator = () => {
       <Stack.Screen name="MainTabs" component={MainNavigator} />
       <Stack.Screen name="CreateTransaction" component={CreateTransactionScreen} />
       <Stack.Screen name="profile" component={ProfileScreen} />
+      <Stack.Screen name="SecuritySettings" component={SecuritySettings} />
       <Stack.Screen name="Token2FA" component={Token2FA} />
     </Stack.Navigator>
   );
